d9faa3b463ab7e46f54e37529acd80e610b235e6
Giovanni Sampaio <giovannibruno1@gmail.com>
2017-06-24 22:09:25 -0300

    Set up sqlite.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CS50-Final-Project
-Final project required to complete CS50 on edx
+Final project required to complete CS50 on edxUsing Python,  Flask and MySQL.
+
--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask
+from flask import g
+import sqlite3
+
+DATABASE = '/app.db'
+
+def  get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
 app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/")
 def main():
